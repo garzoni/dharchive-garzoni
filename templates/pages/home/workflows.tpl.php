f038e7d86d69f1f1ccab0211e894e54e5298e607
Maud Ehrmann <maud.ehrmann@epfl.ch>
2024-09-04 09:19:01 +0200

    Updated video iframes workflows.tpl.php

--- templates/pages/home/workflows.tpl.php
+++ templates/pages/home/workflows.tpl.php
@@ -5,18 +5,18 @@
         <div class="ui padded text container segment">
             <p>After having searched and exported a Garzoni annotation sub-set of interest, one still needs to work with this sub-set in order to answer her research questions.</p>
             <p>Here we propose a set of videos that demonstrate how to use the <a href='https://www.knime.com/knime-software/knime-analytics-platform'>KNIME Analytics Platform</a>, an open source software for data science.</p>
-
-            <h3>Recipe 1: Person Relations</h3>
-            <iframe width="640" height="360" src="https://tube.switch.ch/embed/d2e21b47" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-
+             <h3>Recipe 1: Person Relations</h3>
+            <!--<iframe width="640" height="360" src="https://tube.switch.ch/embed/d2e21b47" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>-->
+            <iframe id="kmsembed-0_sbep83qo" width="608" height="402" src="https://mediaspace.epfl.ch/embed/secure/iframe/entryId/0_sbep83qo/uiConfId/23448972/pbc/49179/st/0" class="kmsembed" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" referrerPolicy="no-referrer-when-downgrade" sandbox="allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation" frameborder="0" title="Garzoni Project - KNIME Recipe 1: Person relations "></iframe>
             <h3>Recipe 2: Apprentice Geographical Origins</h3>
-            <iframe width="640" height="360" src="https://tube.switch.ch/embed/147e1a4d" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-
+            <!--<iframe width="640" height="360" src="https://tube.switch.ch/embed/147e1a4d" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>-->
+            <iframe id="kmsembed-0_b644f66w" width="608" height="402" src="https://mediaspace.epfl.ch/embed/secure/iframe/entryId/0_b644f66w/uiConfId/23448972/pbc/49179/st/0" class="kmsembed" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" referrerPolicy="no-referrer-when-downgrade" sandbox="allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation" frameborder="0" title="Garzoni Project - KNIME Recipe 2: Apprentice Geographical Origins "></iframe>
             <h3>Recipe 3: Master Professions</h3>
-            <iframe width="640" height="360" src="https://tube.switch.ch/embed/c477b3ba" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-
+            <!--<iframe width="640" height="360" src="https://tube.switch.ch/embed/c477b3ba" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>-->
+           <iframe id="kmsembed-0_9zhy13nk" width="608" height="402" src="https://mediaspace.epfl.ch/embed/secure/iframe/entryId/0_9zhy13nk/uiConfId/23448972/pbc/49179/st/0" class="kmsembed" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" referrerPolicy="no-referrer-when-downgrade" sandbox="allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation" frameborder="0" title="Garzoni Project - KNIME Recipe 3: Master professions"></iframe>
             <h3>Recipe 4: Widow Mistress Professions</h3>
-            <iframe width="640" height="360" src="https://tube.switch.ch/embed/91094116" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            <!--<iframe width="640" height="360" src="https://tube.switch.ch/embed/91094116" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>-->
+            <iframe id="kmsembed-0_5g2rulbt" width="608" height="402" src="https://mediaspace.epfl.ch/embed/secure/iframe/entryId/0_5g2rulbt/uiConfId/23448972/pbc/49179/st/0" class="kmsembed" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" referrerPolicy="no-referrer-when-downgrade" sandbox="allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation" frameborder="0" title="Garzoni Project - KNIME Recipe 4: Widow mistress professions"></iframe>          
         </div>
     </div>
 </div>
